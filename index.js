bba7d6fd7c6ac145babec10ad68e11b10744578c
Joab Owala <owalajoab@gmail.com>
2023-03-14 14:52:14 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate"){
+    return `This Saturday, I want to ${fun}!`
+}
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(another ="*",msg="a dedicated programmer"){
+    const inner =function(msg = "special"){
+        return `You are ${another}${msg}${another}!`
+    }
+    return inner;
+}
